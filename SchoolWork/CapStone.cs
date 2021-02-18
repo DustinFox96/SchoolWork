82c8bc33db36457ec596e35606a10067f484e937
Dustin Fox <dustinfox1996@gmail.com>
2021-02-18 11:56:10 -0500

    Add project files.

---
+++ SchoolWork/CapStone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolWork {
+    class CapStone : IPrintable {
+
+        public DateTime CertificationDate { get; set; }
+        public int Attempts { get; set; }
+        public int Score { get; set; }
+
+        public void Print() {
+            var msg = $"CAPSTONE: score: {Score}, on attempt {Attempts} {CertificationDate}";
+            Console.WriteLine(msg);
+        }
+    }
+}
---
+++ SchoolWork/IPrintable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolWork {
+
+    interface IPrintable {
+        void Print();
+
+    }
+}
